60041543a966727a4c75a3ec5ee5b128bc3cce3a
Jokinryou <null1219@gmail.com>
2013-04-27 16:46:56 +0800

    add title to item

--- autohome/autohome/spiders/autohome_spider.py
+++ autohome/autohome/spiders/autohome_spider.py
@@ -20,13 +20,12 @@ class AutohomeSpider(BaseSpider):
         hxs = HtmlXPathSelector(response)
 
         encoding = chardet.detect(response.body)['encoding']
-        print encoding
         if encoding != 'utf-8':
             response.body = response.body.decode(encoding,'replace').encode('utf-8')
 
         #通过XPath选出新闻页面的URL
         cat_urls = hxs.select('//html/body/div/div[3]/div[2]/div[2]/div/span[2]/a/@href').extract()
-        print 'cat_urls = ', cat_urls
+        #print 'cat_urls = ', cat_urls
         for url in cat_urls:
             item = AutohomeItem()
             item['link'] = url
@@ -36,24 +35,16 @@ class AutohomeSpider(BaseSpider):
 
     def parse_content(self, response):
         encoding = chardet.detect(response.body)['encoding']
-        print encoding
         if encoding != 'utf-8':
             response.body = response.body.decode(encoding,'replace').encode('utf-8')
 
         hxs = HtmlXPathSelector(response)
-        content = hxs.select('//html/body/div[4]/div[4]/div/div')
+        #content = hxs.select('//html/body/div[4]/div[4]/div/div')
 
         contents = hxs.select('//div[@class="article"]')
-        item = AutohomeItem()
+        item = response.meta['item']
         for content in contents:
             item['title'] = content.select('h1/text()').extract()
             item['date'] = content.select('div[@class="article_info"]/span[1]/text()').extract()
             item['content'] = content.select('div[@class="article_con"]/p/text()|div[@class="article_con"]/p/a/text()').extract()
-        return item
-
-        #item = AutohomeItem()
-        #item['title'] = content.select('h1/text()').extract()
-        #item['date'] = content.select('div/span/text').extract()
-        #item['content'] = content.select('div[3]/p/text()').extract()
-
         return item
